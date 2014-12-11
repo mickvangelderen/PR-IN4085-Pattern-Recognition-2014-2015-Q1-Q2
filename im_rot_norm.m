6c078aaad6cda7dced2344d001fa76b3731c00b7
Mick van Gelderen <mickvangelderen@gmail.com>
2014-12-11 20:21:12 +0100

    Added rotation normalisation mapping

---
+++ im_rot_norm.m
@@ -0,0 +1,45 @@
+% IM_ROT_NORM
+% normalize images by rotating them upwards if eccentricity and orientation
+% are between bounds
+function b = mapping_example(varargin)
+    argin = shiftargin(varargin,'scalar');
+    argin = setdefaults(argin, [], 0.5);
+    
+    if mapping_task(argin, 'definition')
+        b = define_mapping(argin, 'fixed');
+        b = setname(b, 'Mapping Example');
+        return
+    end
+    
+    [a, scale] = deal(argin{:});
+    
+    if isdataset(a)
+        error('Command cannot be used for datasets as it may change image size')
+    end
+    
+    if isdatafile(a)
+        isobjim(a); % assert that a contains image objects
+        b = filtim(a, mfilename, argin{2:end});
+        return
+    end
+    
+    % convert dip images to double matrix
+    if isa(a, 'dip_image'); a = double(a); end
+    
+    if ~isa(a, 'double'); error('Unknown data type'); end
+    
+    props = regionprops(a, {'eccentricity', 'orientation'});
+    ecc = props.Eccentricity;
+    ori = props.Orientation;
+    
+    % bring orientation range from [-90, 90] to [0, 180]
+    if ori < 0; ori = ori + 180; end
+    
+    % check if eccentricity is high enough and angle doesn't deviate from the up
+    % direction too much. 
+    if ecc > .8 && abs(ori - 90) < 50
+        b = imrotate(a, 90 - ori, 'bilinear', 'loose');
+    else
+        b = a;
+    end
+return
