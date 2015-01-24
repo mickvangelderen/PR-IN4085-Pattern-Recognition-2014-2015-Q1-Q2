b4f6f5673af857a5ee7287764dc83960c6a135a5
Bastiaan Grisel <bastiaan.grisel@gmail.com>
2015-01-24 16:18:12 +0100

    Extended search for holy Grail

--- holy_grail.m
+++ holy_grail.m
@@ -13,12 +13,11 @@ extend_path
 %     'preprocess_speckle_rot_box0_gauss2_stretch', ...
 %     'preprocess_speckle_rot_box0_gauss4_stretch' ...
 % };
-classifiers = {qdc};
-preprocessing_functions = { ...
-    'preprocess_speckle_gauss0_fill'};
+classifiers = {ldc, qdc};
+preprocessing_functions = { 'preprocess_speckle_gauss0_fill', 'preprocess_speckle_gauss0_stretch' };
 objects_per_class = 10;
 max_error = 0.25;
-nrep = 2;
+nrep = 4;
 pca_sizes = 1:10:100;
 images = prnist(0:9, 1:1000);
 
@@ -33,7 +32,7 @@ for pre_pr_func_idx = 1:length(preprocessing_functions)
     func = str2func(name);
     a = func(images);
 
-%     for n = 1:nrep
+    for n = 1:nrep
         [trn, tst] = gendat(a, ones(1, nclasses) * objects_per_class);
 
         nfeatures = size(trn,2);
@@ -45,24 +44,22 @@ for pre_pr_func_idx = 1:length(preprocessing_functions)
             e = tst * w * testc;
 
             for i = 1:size(e,2)
-                err(pca_size, pre_pr_func_idx, i) = e(i);
+                err(pca_size, pre_pr_func_idx, i, n) = e(i);
             end
         end
-%     end
+    end
 end
 
 % Covert error matrix to a more matlab style-array
 means = [];
+stds = [];
 
 for pca_size = pca_sizes
-    means = [means; pca_size reshape(err(pca_size,:,:), [1 nclassifiers * npreprocessing])];
+    means = [means; pca_size reshape(mean(err(pca_size,:,:,:),4), [1 nclassifiers * npreprocessing])];
+    stds  = [stds; pca_size reshape(std(err(pca_size,:,:,:),0,4), [1 nclassifiers * npreprocessing])];
 end
 
-% for pca_size = pca_sizes
-%     means = [means; pca_size reshape(mean(err(pca_size,:,:,:),3), [1 nclassifiers * npreprocessing])];
-%     stds  = [stds; pca_size reshape(std(err(pca_size,:,:,:),0,3), [1 nclassifiers * npreprocessing])];
-% end
-
+% Create a legend
 legend_str = [];
 for class_name = arrayfun(@(x) getname(x), classifiers)
     for pr_fn_name = preprocessing_functions
@@ -71,12 +68,11 @@ for class_name = arrayfun(@(x) getname(x), classifiers)
 end
 
 hold on;
-    plot(means(:,1), means(:,2:end))
-%     errorbar(repmat(pca_sizes,nclassifiers*npreprocessing,1)', means(:,2:end), stds(:,2:end))
+    errorbar(repmat(pca_sizes,nclassifiers*npreprocessing,1)', means(:,2:end), stds(:,2:end))
     plot([min(means(:,1)) max(means(:,1))], [max_error max_error], ':b');
     legend(legend_str, 'Interpreter', 'none');
     axis([min(pca_sizes) max(pca_sizes) 0 1]);
-    gca.XTick = min(pca_sizes):10:max(pca_sizes);
+
     xlabel('Number of PCA Components');
     ylabel('Classification error');
 hold off;
